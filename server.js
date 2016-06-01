30a51cfe410c292c9acb5b375c7853814917a73d
danlangridge <dlangridge@gmail.com>
2016-06-01 15:37:57 -0700

    server now serves public resources / added res from old dashboard

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ var home = "./"
 var server = http.createServer( (req, res) => {
 
     console.log("requested: ".concat(req.url));
-		
+    
     if (req.url == '/') {
+
 	fs.readFile(global.appRoot.concat("/public/index.html"), (err, data) => {
 	    res.setHeader("Content-Type", "text/html");
 	    if (err != null) {
@@ -18,8 +19,28 @@ var server = http.createServer( (req, res) => {
 	    console.log(data);
 	    res.end(data);
 	});
+    } else if (req.url.indexOf("public") >= 0) {
+
+	fs.readFile(global.appRoot.concat(req.url), (err, data) => {
+	    if ( req.url.indexOf("img") >= 0 ) {
+		res.setHeader("Content-Type", "image/gif");
+	    } else if (req.url.indexOf("js") >= 0 ) {
+		res.setHeader("Content-Type", "text/javascript");
+	    } else if (req.url.indexOf("css") >= 0 ) {
+		res.setHeader("Content-Type", "text/css");
+	    } else {
+		res.setHeader("Content-Type", "text/html");
+	    }
+	    
+	    if (err != null) {
+		    console.log(err);
+		}
+		console.log(data);
+		res.end(data);
+	    });
     } else {
 	console.log("requested unknown resource:".concat(req.url));
+	res.end();
     }
 });
 
